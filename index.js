0b766e85db187ae0aa831f510246d8fe1fc9086d
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-26 11:55:27 +0200

    Parses de data from the API and display the temp using metric system

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const request = require("request");
 
 let apiKey = '5baf613db7b6a184b1117c2c08d79482';
 let city = 'joinville';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 
 request(url, function (err, response, body) {
   if (err) {
     console.log("error:", error);
   } else {
-    console.log("body:", body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
