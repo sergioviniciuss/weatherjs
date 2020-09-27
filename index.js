358d5ff4eb8dce78b12a8ec7fae9db9962a89876
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-27 11:42:11 +0200

    Refactors code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const request = require("request");
 
-let apiKey = "5baf613db7b6a184b1117c2c08d79482";
-let city = process.argv[2] || "joinville";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const apiKey = "5baf613db7b6a184b1117c2c08d79482";
+const city = process.argv[2] || "joinville";
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 request(url, function (err, response, body) {
   if (err) {
     console.log("error:", error);
   } else {
-    let weather = JSON.parse(body);
-    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    const weather = JSON.parse(body);
+    const message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
     console.log(message);
   }
 });
