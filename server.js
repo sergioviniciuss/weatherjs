958030427ebcac66d3b03a869b41f00bdc15a4f3
sergioviniciuss <sergioviniciuss@gmail.com>
2020-09-27 11:54:03 +0200

    Makes the API calls from the server and display the value on the client

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
+
+let apiKey = "5baf613db7b6a184b1117c2c08d79482";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +18,22 @@ app.listen(3000, () => {
 });
 
 app.post("/", (req, res) => {
-  res.render("index");
-  console.log(req.body.city);
+  const city = req.body.city;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  request(url, (err, response, body) => {
+    if (err) {
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      const weather = JSON.parse(body);
+
+      if (weather.main === undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        const weatherResponse = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', { weather: weatherResponse, error: null })
+      }
+    }
+  });
+
 });
